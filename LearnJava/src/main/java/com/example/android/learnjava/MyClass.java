957c6cfd702e17355dcf625edabd47241bc93e2a
Abacus7 <vladhuz@yahoo.com>
2020-07-16 18:58:44 -0400

    feat: added content provider - StorageProvider.java
    added StorageProvider.java draft, still need to refacture couple fragments of code, updated manifest with StorageProvider data as well.

---
+++ LearnJava/src/main/java/com/example/android/learnjava/MyClass.java
@@ -0,0 +1,8 @@
+package com.example.android.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("Hey I am a programmer!!! Lol ");
+    }
+}
